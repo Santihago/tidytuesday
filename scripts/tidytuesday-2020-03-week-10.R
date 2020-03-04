2a642504e1d2b614010e88d3a9feabea994fb61b
Santiago Muñoz <santimz@gmail.com>
2020-03-04 23:29:32 +0100

    script header

--- scripts/tidytuesday-2020-03-week-10.R
+++ scripts/tidytuesday-2020-03-week-10.R
@@ -1,4 +1,12 @@
-# Get the Data
+# TidyTuesday Week 10
+# Author: Santiago Muñoz Moldes
+# Date: March 4th, 2020
+
+# Dataset information: https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-03-03/readme.md
+# Dataset source: https://www.washingtonpost.com/graphics/2020/sports/capitals/ovechkin-700-goals
+
+library(tidyverse)
+library(ggtext)
 
 top_250 <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/top_250.csv')
 
